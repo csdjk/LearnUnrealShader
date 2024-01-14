d0183df0dbdf28bac522dcb2421ba20c39d2e590
csdjk <1076984375@qq.com>
2024-01-14 22:27:06 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# LcL-LearnUnrealShader
+# LearnUnrealShader
+
 记录学习Unreal Shader过程中实现的一些Demo。
---
+++ Source/LearnUnrealShader/LearnUnrealShader.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LearnUnrealShader.h"
+#include "Modules/ModuleManager.h"
+#include "Misc/Paths.h"
+
+
+
+void FLearnUnrealShaderModule::StartupModule()
+{
+	/// @note 和上一步名称对应，Shaders 文件夹
+	FString ShaderDirectory = FPaths::Combine(FPaths::ProjectDir(), TEXT("Shaders"));
+	AddShaderSourceDirectoryMapping(TEXT("/Shaders"), ShaderDirectory);
+
+}
+
+void FLearnUnrealShaderModule::ShutdownModule()
+{
+}
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FLearnUnrealShaderModule,  LearnUnrealShader, "LearnUnrealShader");
+
